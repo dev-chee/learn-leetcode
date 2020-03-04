98b61fa5257dbed5960c0cbbd270af7fb835fbf2
Chee <dev_chee@outlook.com>
2020-03-04 17:36:21 +0800

    Random povit quick sort

--- src/algorithms/quick_sort.rs
+++ src/algorithms/quick_sort.rs
@@ -1,7 +1,13 @@
+use rand::Rng;
+
 pub fn quick_sort(values: &mut [i32]) {
     if values.len() <= 1 {
         return;
     }
+
+    let mut rng = rand::thread_rng();
+    let idx = rng.gen_range(0, values.len());
+    values.swap(0, idx);
 
     let mut p_idx = 0;
     let p = values[p_idx];
@@ -33,8 +39,8 @@ mod tests {
 
     #[test]
     fn test() {
-        let mut v = [2, 3, 10, 7, 15, 22, 18, 23, 76, 100, 38, 92];
+        let mut v = [2, 3, 10, 7, 15, 22, 77, 11, 32, 41, 18, 23, 76, 100, 38, 92];
         quick_sort(&mut v);
-        assert_eq!(v, [2, 3, 7, 10, 15, 18, 22, 23, 38, 76, 92, 100]);
+        assert_eq!(v, [2, 3, 7, 10, 11, 15, 18, 22, 23, 32, 38, 41, 76, 77, 92, 100]);
     }
 }
